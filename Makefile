6c27cb292fa7fa88c9d1b9f019085d91101dcd48
avolent <will@slamped.com>
2022-10-26 03:43:09 +0000

    Add argument support

--- Makefile
+++ Makefile
@@ -1,8 +1,10 @@
 # Global command variables
 COMPOSE_BUILD_NETTEST = docker compose build nettest
 COMPOSE_RUN_NETTEST = docker compose run --rm nettest
-COMPOSE_RUN_BASH = docker compose run --rm nettest bash
+COMPOSE_RUN_BASH = docker compose run --rm --entrypoint bash nettest
 DOCKER_CLEAN = docker compose down --remove-orphans && docker image prune && docker network prune
+
+ARGUMENTS = -f csv -A
 
 .PHONY: run_test
 run_test: build run
@@ -11,11 +13,17 @@ run_test: build run
 
 # Builds the docker image using docker compose.
 build: 
-	$(COMPOSE_BUILD_NETTEST)
+	$(COMPOSE_BUILD_NETTEST) 
 
 # Runs the docker images for using docker compose.
 run:
-	$(COMPOSE_RUN_NETTEST)
+	$(COMPOSE_RUN_NETTEST) net-test.py $(ARGUMENTS)
+
+help:
+	$(COMPOSE_RUN_NETTEST) net-test.py -h
+
+servers:
+	$(COMPOSE_RUN_NETTEST) net-test.py -L
 
 # Enter a bash terminal within the docker image.
 bash:
--- app/net-test.py
+++ app/net-test.py
@@ -1,10 +1,8 @@
-import os, subprocess
+import os, subprocess, sys
 
 # Global Variables
 HEADER = '"server name","server id","idle latency","idle jitter","packet loss","download","upload","download bytes","upload bytes","share url","download server count","download latency","download latency jitter","download latency low","download latency high","upload latency","upload latency jitter","upload latency low","upload latency high","idle latency low","idle latency high"\n'
 DIR_PATH = os.path.dirname(os.path.realpath(__file__))
-
-
 # File write function
 def file_write(input: str):
     """Write string to output file.
@@ -15,29 +13,34 @@ def file_write(input: str):
     f.write(input)
     f.close()
 
-def speedtest() -> str:
+def speedtest(args: list) -> str:
     """Run the speedtest command with the output format of CSV.
     More details of command can be seen with speedtest --help
 
     :return results: str - Return the results of the speedtest.
     """
     print("Starting speedtest.")
-    results = subprocess.run(['speedtest', '-f', 'csv', '-A'], capture_output=True, text=True)
+    # Update argument one to Speedtest CLI name.
+    args[0] = "speedtest"
+    print(args)
+    results = subprocess.run(args, capture_output=True, text=True)
+    # If license has not been accepted yet. go ahead and accept it.
+    if "--accept-license" in results.stderr:
+        args.append("--accept-license")
+        results = subprocess.run(args, capture_output=True, text=True)
+    if not set(["CSV", "csv"]).isdisjoint(set(args)): 
+        file_write(results.stdout)
     print(results.stdout)
-    print("Test complete.")
     return results
 
-def main():
-    """Main speedtest function
-    Will run all the other functions from here
-    Checks if output file exists, if not will create one.
-    Writes results to the output file on completion of speedtest.
-    """
+def main(args: list):
+    """Main speedtest function"""
+    # Checks for output.csv file, creates if not available.
     if not os.path.exists(f"{DIR_PATH}/output.csv"):
         print("CSV file doesnt exist. Creating!")
         file_write(HEADER)
-    results = speedtest()
-    file_write(results.stdout)
+    # Runs speedtest function and sets results to a variable
+    speedtest(args)
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv)
