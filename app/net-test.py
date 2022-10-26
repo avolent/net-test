5400f5d9fd9e25779697f7ce8f123f3c3bce9a4a
avolent <will@slamped.com>
2022-10-26 04:28:24 +0000

    Add timestamp to output

--- app/net-test.py
+++ app/net-test.py
@@ -1,8 +1,11 @@
 import os, subprocess, sys
+from datetime import datetime
 
 # Global Variables
-HEADER = '"server name","server id","idle latency","idle jitter","packet loss","download","upload","download bytes","upload bytes","share url","download server count","download latency","download latency jitter","download latency low","download latency high","upload latency","upload latency jitter","upload latency low","upload latency high","idle latency low","idle latency high"\n'
+HEADER = '"timestamp","server name","server id","idle latency","idle jitter","packet loss","download","upload","download bytes","upload bytes","share url","download server count","download latency","download latency jitter","download latency low","download latency high","upload latency","upload latency jitter","upload latency low","upload latency high","idle latency low","idle latency high"\n'
 DIR_PATH = os.path.dirname(os.path.realpath(__file__))
+TIMESTAMP = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
 # File write function
 def file_write(input: str):
     """Write string to output file.
@@ -22,14 +25,13 @@ def speedtest(args: list) -> str:
     print("Starting speedtest.")
     # Update argument one to Speedtest CLI name.
     args[0] = "speedtest"
-    print(args)
     results = subprocess.run(args, capture_output=True, text=True)
     # If license has not been accepted yet. go ahead and accept it.
     if "--accept-license" in results.stderr:
         args.append("--accept-license")
         results = subprocess.run(args, capture_output=True, text=True)
     if not set(["CSV", "csv"]).isdisjoint(set(args)): 
-        file_write(results.stdout)
+        file_write(f'"{TIMESTAMP}", {results.stdout}')
     print(results.stdout)
     return results
 
